49c6aeb34d6215c6addfeb787412d4659db5c7aa
Dmitry Bubenok <dima@bubenok.ru>
2024-12-18 08:54:25 +0700

    add index

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fontom | PHP library for handling font files</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Fontom</h1>
+        <h3>PHP library for handling font files</h3>
+    </div>
+
+    <?php
+        
+    ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
